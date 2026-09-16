0e1a85728e1af990730a3ccee358f111808b3fdb
EvilBarry <barrysactualemail@gmail.com>
2026-09-16 10:48:25 +0200

    added assignment
    oefenopdracht 4

--- Oefenopdrachten/Opdracht 4/sketch.js
+++ Oefenopdrachten/Opdracht 4/sketch.js
@@ -1,7 +1,82 @@
+let counter = 0
+let colorpicker = 0
+let eightballX = 0
+let eightballY = 0
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 400);
 }
 
 function draw() {
   background(220);
+  counter++
+  text("1. Houd B in om een blokje te laten verschijnen.", 20, 20)
+  text("2. Druk op spatie om het getal op 0 te zetten.", 20, 120)
+  text(counter, 60, 180)
+  text("3. Druk op enter om van rood -> groen -> oranje te gaan.", 20, 240)
+  text("4. Beweeg de eightball met WASD of de pijltjestoetsen.", 360, 20)
+  if(counter >= 500){
+    counter = 0
+  }
+  if(keyIsDown (66)){
+    square(60, 40, 60)
+  }
+  if(colorpicker >= 3){
+    colorpicker = 0
+  }
+  noStroke()
+  fill("#a8a8a8")
+  rect(20, 245, 30, 80)
+  rect(27, 315, 15, 40)
+
+  if(colorpicker < 1){
+    fill("#f01a1a")
+  circle(35, 260, 20)
+  fill("#000000")
+  circle(35, 285, 20)
+  circle(35, 310, 20)
+  }
+  if(colorpicker >= 1 && colorpicker < 2){
+    fill("#000000")
+  circle(35, 260, 20)
+  circle(35, 310, 20)
+  fill("#e3c21b")
+  circle(35, 285, 20)
+  }
+  if(colorpicker >= 2 && colorpicker < 3){
+    fill("#000000")
+  circle(35, 260, 20)
+  circle(35, 285, 20)
+  fill("#19db29")
+  circle(35, 310, 20)
+  }
+  fill("#000000")
+  circle(eightballX + 360, eightballY + 60, 50)
+  fill("#ffffff")
+  circle(eightballX + 360, eightballY + 60, 35)
+  fill("#000000")
+  text("8", eightballX + 355, eightballY + 65)
+  fill("#ffffff")
+  stroke("#000000")
+  if(keyIsDown (87) || keyIsDown (38)){
+    eightballY--
+  }
+  if(keyIsDown (83) || keyIsDown (40)){
+    eightballY++
+  }
+  if(keyIsDown (65) || keyIsDown (37)){
+    eightballX--
+  }
+  if(keyIsDown (68) || keyIsDown (39)){
+    eightballX++
+  }
+}
+
+function keyPressed() {
+  if (keyCode == 32) {
+    counter = 0
+  }
+  if (keyCode == 13) {
+    colorpicker++
+  }
 }
--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,7 +1,21 @@
+let MovementRight = -100 //mijn variabele MovementRight, met -100 als startgetal
+let MovementLeft = 900 //mijn variabele MovementLeft, met 900 als startgetal
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
-  background(220);
+  background("#00AFFF");
+  MovementRight++ //voeg 1 aan de waarde van MovementRight voor elke frame
+  MovementLeft -- //trek 1 af van de waarde van MovementLight voor elke frame
+  if(MovementRight > 900){ //als de waarde van MovementRight boven de 900 is,
+    MovementRight = -100   //dan springt de waarde terug naar het begingetal,
+  }
+  if(MovementLeft < -100){ //als de waarde van MovementLeft onder de -100 is,
+    MovementLeft = 900   //dan springt de waarde terug naar het begingetal.
+  }
+  circle(MovementRight, 200, 50)
+  circle(MovementLeft, 600, 50)
+  text(mouseX, 0, 40)
+  text(mouseY, 0, 60)
 }
