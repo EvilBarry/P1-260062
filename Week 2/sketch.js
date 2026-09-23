2d2a79cab887e62937e0f8857e67248caff8cd57
EvilBarry <barrysactualemail@gmail.com>
2026-09-23 12:41:00 +0200

    added assignment

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,19 +1,49 @@
-let MovementRight = -100 //mijn variabele MovementRight, met -100 als startgetal
-let MovementLeft = 900 //mijn variabele MovementLeft, met 900 als startgetal
+let MovementRight = -40
+
 function setup() {
   createCanvas(800, 800);
 }
 
 function draw() {
-  background("#00AFFF");
-  MovementRight++ //voeg 1 aan de waarde van MovementRight voor elke frame
-  MovementLeft -- //trek 1 af van de waarde van MovementLight voor elke frame
-  if(MovementRight > 900){ //als de waarde van MovementRight boven de 900 is,
-    MovementRight = -100   //dan springt de waarde terug naar het begingetal,
+  background("#3ebcee");
+  MovementRight++
+  noStroke()
+  fill("#cdb6379e")
+  circle(MovementRight, 60, 120)
+  fill("#e0881c9e")
+  circle(MovementRight, 60, 90)
+  fill("#ffe760")
+  circle(MovementRight, 60, 80)
+  fill("#525252")
+  triangle(0, 800, 350, 800, 175, 300)
+  triangle(230, 800, 580, 800, 405, 300)
+  fill("#797979")
+  triangle(115, 800, 465, 800, 290, 200)
+  fill("#35d966")
+  rect(0, 600, 800, 200)
+  ellipse(650, 800, 400, 1200)
+  fill("#805a22")
+  rect(50, 425, 20, 200)
+  rect(250, 425, 20, 200)
+  fill("#4ca266")
+  circle(60, 425, 150)
+  circle(260, 425, 150)
+  fill("#ffe760")
+  square(610, 150, 80)
+  fill("#b52525")
+  triangle(610, 150, 690, 150, 650, 100)
+  fill("#525252")
+  rect(0, 650, 800, 150)
+  fill("#805a22")
+  rect(50, 425, 20, 200)
+  rect(250, 425, 20, 200)
+  rect(750, 600, 20, 200)
+  fill("#4ca266")
+  circle(60, 425, 150)
+  circle(260, 425, 150)
+  circle(760, 600, 150)
+  if(MovementRight >= 840){
+    MovementRight = -40
   }
-  if(MovementLeft < -100){ //als de waarde van MovementLeft onder de -100 is,
-    MovementLeft = 900   //dan springt de waarde terug naar het begingetal.
-  }
-  circle(MovementRight, 200, 50)
-  circle(MovementLeft, 600, 50)
+  
 }
--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,7 +1,387 @@
+//mijn variabelen. "Square[letter]_State" staat voor de kleur van de blokjes. ik had deze beter
+//kunnen benoemen maar dat boeit mij niet.
+let SquareA = 0
+let SquareA_State = "#3b3b3b"
+let SquareB = 0
+let SquareB_State = "#3b3b3b"
+let SquareC = 0
+let SquareC_State = "#3b3b3b"
+let SquareD = 0
+let SquareD_State = "#3b3b3b"
+let SquareE = 0
+let SquareE_State = "#3b3b3b"
+let SquareF = 0
+let SquareF_State = "#3b3b3b"
+let SquareG = 0
+let SquareG_State = "#3b3b3b"
+let SquareH = 0
+let SquareH_State = "#3b3b3b"
+let SquareI = 0
+let SquareI_State = "#3b3b3b"
+let turn = 0
+let P1Win = 0
+let P2Win = 0
+let P1Score = 0
+let P2Score = 0
+let Restart = 0
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1200, 800);
+  textSize(50)
+  noStroke()
 }
 
 function draw() {
   background(220);
+  fill("#000000")
+  text(P1Score, 950, 400)
+  text("-", 1050, 400)
+  text(P2Score, 1150, 400)
+  square(50, 50, 700, 50)
+  fill(SquareA_State)
+  square(325, 325, 150)
+  fill(SquareB_State)
+  square(325, 525, 150)
+  fill(SquareC_State)
+  square(325, 125, 150)
+  fill(SquareD_State)
+  square(125, 325, 150)
+  fill(SquareE_State)
+  square(525, 325, 150)
+  fill(SquareF_State)
+  square(525, 525, 150)
+  fill(SquareG_State)
+  square(125, 525, 150)
+  fill(SquareH_State)
+  square(125, 125, 150)
+  fill(SquareI_State)
+  square(525, 125, 150)
+
+  if (SquareA == 0){
+    SquareA_State = "#3b3b3b"
+  }
+  if (SquareA == 2){
+    SquareA_State = "#2a06f3"
+  }
+  if (SquareA == 3){
+    SquareA_State = "#ff1900"
+  }
+  if (SquareB == 0){
+    SquareB_State = "#3b3b3b"
+  }
+  if (SquareB == 2){
+    SquareB_State = "#2a06f3"
+  }
+  if (SquareB == 3){
+    SquareB_State = "#ff1900"
+  }
+  if (SquareC == 0){
+    SquareC_State = "#3b3b3b"
+  }
+  if (SquareC == 2){
+    SquareC_State = "#2a06f3"
+  }
+  if (SquareC == 3){
+    SquareC_State = "#ff1900"
+  }
+  if (SquareD == 0){
+    SquareD_State = "#3b3b3b"
+  }
+  if (SquareD == 2){
+    SquareD_State = "#2a06f3"
+  }
+  if (SquareD == 3){
+    SquareD_State = "#ff1900"
+  }
+  if (SquareE == 0){
+    SquareE_State = "#3b3b3b"
+  }
+  if (SquareE == 2){
+    SquareE_State = "#2a06f3"
+  }
+  if (SquareE == 3){
+    SquareE_State = "#ff1900"
+  }
+  if (SquareF == 0){
+    SquareF_State = "#3b3b3b"
+  }
+  if (SquareF == 2){
+    SquareF_State = "#2a06f3"
+  }
+  if (SquareF == 3){
+    SquareF_State = "#ff1900"
+  }
+  if (SquareG == 0){
+    SquareG_State = "#3b3b3b"
+  }
+  if (SquareG == 2){
+    SquareG_State = "#2a06f3"
+  }
+  if (SquareG == 3){
+    SquareG_State = "#ff1900"
+  }
+  if (SquareH == 0){
+    SquareH_State = "#3b3b3b"
+  }
+  if (SquareH == 2){
+    SquareH_State = "#2a06f3"
+  }
+  if (SquareH == 3){
+    SquareH_State = "#ff1900"
+  }
+  if (SquareI == 0){
+    SquareI_State = "#3b3b3b"
+  }
+  if (SquareI == 2){
+    SquareI_State = "#2a06f3"
+  }
+  if (SquareI == 3){
+    SquareI_State = "#ff1900"
+  }
+
+  if (SquareA == 2 && SquareB == 2 && SquareC == 2){
+    P1Win = 1
+  }else if (SquareA == 2 && SquareD == 2 && SquareE == 2){
+    P1Win = 1
+  }else if (SquareH == 2 && SquareC == 2 && SquareI == 2){
+    P1Win = 1
+  }else if (SquareG == 2 && SquareB == 2 && SquareF == 2){
+    P1Win = 1
+  }else if (SquareH == 2 && SquareA == 2 && SquareF == 2){
+    P1Win = 1
+  }else if (SquareG == 2 && SquareA == 2 && SquareI == 2){
+    P1Win = 1
+  }else if (SquareH == 2 && SquareD == 2 && SquareG == 2){
+    P1Win = 1
+  }else if (SquareI == 2 && SquareE == 2 && SquareF == 2){
+    P1Win = 1
+  }else if (SquareA == 3 && SquareB == 3 && SquareC == 3){
+    P2Win = 1
+  }else if (SquareA == 3 && SquareD == 3 && SquareE == 3){
+    P2Win = 1
+  }else if (SquareH == 3 && SquareC == 3 && SquareI == 3){
+    P2Win = 1
+  }else if (SquareG == 3 && SquareB == 3 && SquareF == 3){
+    P2Win = 1
+  }else if (SquareH == 3 && SquareA == 3 && SquareF == 3){
+    P2Win = 1
+  }else if (SquareG == 3 && SquareA == 3 && SquareI == 3){
+    P2Win = 1
+  }else if (SquareH == 3 && SquareD == 3 && SquareG == 3){
+    P2Win = 1
+  }else if (SquareI == 3 && SquareE == 3 && SquareF == 3){
+    P2Win = 1
+  }else if (SquareA !== 0 && SquareB !== 0 && SquareC !== 0 && 
+            SquareD !== 0 && SquareE !== 0 && SquareF !== 0 && 
+            SquareG !== 0 && SquareH !== 0 && SquareI !== 0){
+    fill("#414142")
+    text("Draw!", 200, 50)
+  }
+
+  if (P1Win == 1){
+    fill("#3333ff")
+    text("Player 1 won!", 900, 50)
+    P1Score++
+  }
+  if (P2Win == 1){
+    fill("#ff3333")
+    text("Player 2 won!", 900, 50)
+    P2Score++
+  }
+
+  if (P1Win == 1 || P2Win == 1){
+    fill("#535353")
+    rect(950, 500, 200, 100)
+    fill("#000000")
+    text("restart", 1000, 550)
+    Restart = 1
+  }
+
+  if (mouseX >= 325 && mouseX <= 475 && mouseY >= 325
+      && mouseY <= 475 && SquareA !== 2 && SquareA !== 3
+            && P1Win !== 1 && P2Win !== 1){
+  fill("#ffffff")
+  square(325, 325, 150)
+  }else if (mouseX >= 325 && mouseX <= 475 && mouseY >= 525
+            && mouseY <= 675 && SquareB !== 2 && SquareB !== 3
+            && P1Win !== 1 && P2Win !== 1){
+  fill("#ffffff")
+  square(325, 525, 150)
+  }else if (mouseX >= 325 && mouseX <= 475 && mouseY >= 125
+            && mouseY <= 275 && SquareC !== 2 && SquareC !== 3
+            && P1Win !== 1 && P2Win !== 1){
+  fill("#ffffff")
+  square(325, 125, 150)
+  }else if (mouseX >= 125 && mouseX <= 275 && mouseY >= 325
+            && mouseY <= 475 && SquareD !== 2 && SquareD !== 3
+            && P1Win !== 1 && P2Win !== 1){
+  fill("#ffffff")
+  square(125, 325, 150)
+  }else if (mouseX >= 525 && mouseX <= 675 && mouseY >= 325
+            && mouseY <= 475 && SquareE !== 2 && SquareE !== 3
+            && P1Win !== 1 && P2Win !== 1){
+  fill("#ffffff")
+  square(525, 325, 150)
+  }else if (mouseX >= 525 && mouseX <= 675 && mouseY >= 525
+            && mouseY <= 675 && SquareF !== 2 && SquareF !== 3
+            && P1Win !== 1 && P2Win !== 1){
+  fill("#ffffff")
+  square(525, 525, 150)
+  }else if (mouseX >= 125 && mouseX <= 275 && mouseY >= 525
+            && mouseY <= 675 && SquareG !== 2 && SquareG !== 3
+            && P1Win !== 1 && P2Win !== 1){
+  fill("#ffffff")
+  square(125, 525, 150)
+  }else if (mouseX >= 125 && mouseX <= 275 && mouseY >= 125
+            && mouseY <= 275 && SquareH !== 2 && SquareH !== 3
+            && P1Win !== 1 && P2Win !== 1){
+  fill("#ffffff")
+  square(125, 125, 150)
+  }else if (mouseX >= 525 && mouseX <= 675 && mouseY >= 125
+            && mouseY <= 275 && SquareI !== 2 && SquareI !== 3
+            && P1Win !== 1 && P2Win !== 1){
+  fill("#ffffff")
+  square(525, 125, 150)
+  }else {
+    noFill()
+  }
+}
+
+function mouseClicked() {
+    if (mouseButton === LEFT){
+    if (Restart == 1){
+      if (mouseX >= 950 && mouseX <= 1150 && mouseY >= 500 && mouseY <= 600){
+      SquareA = 0
+      SquareA_State = "#3b3b3b"
+      SquareB = 0
+      SquareB_State = "#3b3b3b"
+      SquareC = 0
+      SquareC_State = "#3b3b3b"
+      SquareD = 0
+      SquareD_State = "#3b3b3b"
+      SquareE = 0
+      SquareE_State = "#3b3b3b"
+      SquareF = 0
+      SquareF_State = "#3b3b3b"
+      SquareG = 0
+      SquareG_State = "#3b3b3b"
+      SquareH = 0
+      SquareH_State = "#3b3b3b"
+      SquareI = 0
+      SquareI_State = "#3b3b3b"
+      turn = 0
+      P1Win = 0
+      P2Win = 0
+      Restart = 0 
+      }
+    }
+    if (turn === 0 && SquareA !== 3 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 325 && mouseX <= 475 && mouseY >= 325 && mouseY <= 475){
+        SquareA = 2
+        turn = 1
+      }
+    }
+    if (turn === 0 && SquareB !== 3 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 325 && mouseX <= 475 && mouseY >= 525 && mouseY <= 675){
+        SquareB = 2
+        turn = 1
+      }
+    }
+    if (turn === 0 && SquareC !== 3 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 325 && mouseX <= 475 && mouseY >= 125 && mouseY <= 275){
+        SquareC = 2
+        turn = 1
+      }
+    }
+    if (turn === 0 && SquareD !== 3 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 125 && mouseX <= 275 && mouseY >= 325 && mouseY <= 475){
+        SquareD = 2
+        turn = 1
+      }
+    }
+    if (turn === 0 && SquareE !== 3 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 525 && mouseX <= 675 && mouseY >= 325 && mouseY <= 475){
+        SquareE = 2
+        turn = 1
+      }
+    }
+    if (turn === 0 && SquareF !== 3 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 525 && mouseX <= 675 && mouseY >= 525 && mouseY <= 675){
+        SquareF = 2
+        turn = 1
+      }
+    }
+    if (turn === 0 && SquareG !== 3 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 125 && mouseX <= 275 && mouseY >= 525 && mouseY <= 675){
+        SquareG = 2
+        turn = 1
+      }
+    }
+    if (turn === 0 && SquareH !== 3 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 125 && mouseX <= 275 && mouseY >= 125 && mouseY <= 275){
+        SquareH = 2
+        turn = 1
+      }
+    }
+    if (turn === 0 && SquareI !== 3 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 525 && mouseX <= 675 && mouseY >= 125 && mouseY <= 275){
+        SquareI = 2
+        turn = 1
+      }
+    }
+    if (turn === 1 && SquareA !== 2 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 325 && mouseX <= 475 && mouseY >= 325 && mouseY <= 475){
+        SquareA = 3
+        turn = 0
+      }
+    }
+    if (turn === 1 && SquareB !== 2 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 325 && mouseX <= 475 && mouseY >= 525 && mouseY <= 675){
+        SquareB = 3
+        turn = 0
+      }
+    }
+    if (turn === 1 && SquareC !== 2 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 325 && mouseX <= 475 && mouseY >= 125 && mouseY <= 275){
+        SquareC = 3
+        turn = 0
+      }
+    }
+    if (turn === 1 && SquareD !== 2 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 125 && mouseX <= 275 && mouseY >= 325 && mouseY <= 475){
+        SquareD = 3
+        turn = 0
+      }
+    }
+    if (turn === 1 && SquareE !== 2 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 525 && mouseX <= 675 && mouseY >= 325 && mouseY <= 475){
+        SquareE = 3
+        turn = 0
+      }
+    }
+    if (turn === 1 && SquareF !== 2 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 525 && mouseX <= 675 && mouseY >= 525 && mouseY <= 675){
+        SquareF = 3
+        turn = 0
+      }
+    }
+    if (turn === 1 && SquareG !== 2 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 125 && mouseX <= 275 && mouseY >= 525 && mouseY <= 675){
+        SquareG = 3
+        turn = 0
+      }
+    }
+    if (turn === 1 && SquareH !== 2 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 125 && mouseX <= 275 && mouseY >= 125 && mouseY <= 275){
+        SquareH = 3
+        turn = 0
+      }
+    }
+    if (turn === 1 && SquareI !== 2 && P1Win !== 1 && P2Win !== 1){
+      if (mouseX >= 525 && mouseX <= 675 && mouseY >= 125 && mouseY <= 275){
+        SquareI = 3
+        turn = 0
+      }
+    }
+  }
 }
